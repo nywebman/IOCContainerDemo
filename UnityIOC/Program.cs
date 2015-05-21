0d176867a4c4dc7b28d28d7efe0fe9f116dbeee7
Brad Rosen <brosen@mdsol.com>
2015-05-21 15:39:41 -0400

    begin to configure unity

---
+++ UnityIOC/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace UnityIOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+            container.RegisterType<ICreditCard, MasterCard>();
+            //property injection
+            container.RegisterType<ICreditCard, MasterCard>(new InjectionProperty("chargeCount", 5));
+            //container.RegisterType<ICreditCard, MasterCard>("DefaultCard");
+            //container.RegisterType<ICreditCard, Visa>("BackupCard");
+
+            //var card = new MasterCard();
+            //container.RegisterInstance(card);
+            
+        }
+
+        public class Shopper
+        {
+            private readonly ICreditCard creditCard;
+
+            public Shopper(ICreditCard creditCard)
+            {
+                this.creditCard = creditCard;
+            }
+
+            public void Charge()
+            {
+                var chargeMessage = creditCard.Charge();
+                Console.WriteLine(chargeMessage);
+            }
+        }
+
+        public interface ICreditCard
+        {
+            int chargeCount { get; }
+            string Charge();
+        }
+
+        public class MasterCard : ICreditCard
+        {
+            public int chargeCount { get; set; }
+
+            public string Charge()
+            {
+                chargeCount++;
+                return "swiping the master card";
+            }
+        }
+        public class Visa : ICreditCard
+        {
+            public int chargeCount { get; set; }
+
+            public string Charge()
+            {
+                chargeCount++;
+                return "charging with visa";
+            }
+        }
+    }
+}
