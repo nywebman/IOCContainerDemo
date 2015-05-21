a973344878af07d0e8a138ddb038c7b7572e7b77
Brad Rosen <brosen@mdsol.com>
2015-05-21 12:36:05 -0400

    initial http://www.pluralsight.com/courses/inversion-of-control

---
+++ IOCContainerDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOCContainerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditCard creditCard=new MasterCard();
+            ICreditCard otherCard = new Visa();
+
+            //shopper is not dependant on MasterCard
+            var shopper = new Shopper(otherCard); 
+
+            shopper.Charge();
+            Console.Read();
+        }
+
+    }
+
+    public class Shopper
+    {
+        private readonly ICreditCard creditCard;
+
+        public Shopper(ICreditCard creditCard)
+        {
+            this.creditCard = creditCard;
+        }
+
+        public void Charge()
+        {
+            var chargeMessage=creditCard.Charge();
+            Console.WriteLine(chargeMessage);
+        }
+    }
+
+    public interface ICreditCard
+    {
+        string Charge();
+    }
+
+    public class MasterCard : ICreditCard
+    {
+        public string Charge()
+        {
+            return "swiping the master card";
+        }
+    }
+    public class Visa : ICreditCard
+    {
+        public string Charge()
+        {
+            return "charging with visa";
+        }
+    }
+}
